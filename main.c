307d01f716b5a1e2cddbaff10e5d9f3be8b09042
Vinicius <vinistud@gmail.com>
2020-11-17 20:29:05 +0000

    Só um detalhe

--- main.c
+++ main.c
@@ -9,12 +9,13 @@
 int main(void) {
   setlocale(LC_ALL, "Portuguese");
   char nome[SIZE][50];
-  char email[SIZE] [50];
   int cpf[SIZE];
   int op, op1;
   int assento[10] [10];
-  int linha;
+  static int linha;
+  int fileira;
   int cadastro, pesquisa, cpfPesquisa, i, f, c, lin, col;
+  int s[SIZE];
     
   system("cls"); 
 
@@ -25,11 +26,11 @@ int main(void) {
   
 
     
-  for(f=0 ; f<10; f++)
+  for(lin=0 ; lin<10; lin++)
   {
     printf("\t ");
-		printf("Fileira %d ->",f);
-      for(c=0 ; c<10 ; c++)
+		printf("Fileira %d ->",lin);
+      for(col=0 ; col<10 ; col++)
       {
         printf("|-| ");
         }
@@ -45,11 +46,11 @@ int main(void) {
           printf("\n\n");
           
           
-          for(f=0 ; f<10; f++)
+          for(lin=0 ; lin<10; lin++)
           {
-            for(c=0 ; c<10 ; c++)
+            for(col=0 ; col<10 ; col++)
             {
-              assento [f] [c]=0;
+              assento [lin] [col]=0;
               }
               }
               
@@ -65,83 +66,87 @@ do{//system permite executar o prompt dentro do programa
             scanf("%d", &op1);
             printf("\n\n");
             
+            
             if (op1==4){
-              printf("AS 2 PRIMEIRAS FILEIRAS SÃO RESERVADOS PARA PROFESSORES E CONVIDADOS DO PALESTRANTE! E AS FILEIRAS 2, 3, 4, 5 E 6 SÃO PARA PESSOAS COM NECESSIDADES ESPECIAIS.\n");
+
+              printf("!!! AS 2 PRIMEIRAS FILEIRAS SÃO RESERVADOS PARA PROFESSORES E CONVIDADOS DO PALESTRANTE!!! \n!!! E AS FILEIRAS 2, 3, 4, 5 E 6 SÃO PARA PESSOAS COM NECESSIDADES ESPECIAIS. !!!\n\n");
+              printf("\nSelecione o local desejado:\n");
               printf("\nFileira: ");
               scanf("%d", &lin);
               printf("Coluna: ");
-              scanf("%d",&col);
+              scanf("%d", &col);
               
-            if  (lin>1 & lin<10 && col<10)
-            {
-              if (assento [lin] [col]==0)
+              
+              if  (lin>1 & lin<10 && col<10)
               {
-                assento [lin] [col]=1;
-                printf("\nDigite o nome: ");
-		            scanf("%s", &nome[linha]);
-		            printf("\ndigite o cpf:");
-		            scanf("%d", &cpf[linha]);
-                printf("");              
-                printf("\nDados gravados com sucesso!");
-                }            
-						else
-						{
-							printf("******ATENÇÃO! CADEIRA OCUPADA!******\n\n");
-						}
-            
-            }
-					else
-					{
-						printf("\n******CADEIRA NÃO DISPONÍVEL!******");
-					}
-					printf("\n");
-          printf("------------------------------------------------\n");
-
-
-
-
-
-
-           printf("\t\t\t\t________________________________________\n");
-  printf("\t\t\t\t      |                         |\n");
-  printf("\t\t\t\t      |          PALCO          |\n");
-  printf("\t\t\t\t      |_________________________|\n\n");
-
-          		for(f=0 ; f<10; f++){
-									printf("\t ");
-									printf("Fileira %d ->",f);
-                  for(c=0 ; c<10 ; c++){
-									if(assento[f] [c]==0){
-                    printf("|-| ");
-                    }else
-                    printf("|X| ");
+                if (assento [lin] [col]==0)
+                
+                {
+                  
+                  assento [lin] [col]=1;
+                  printf("\nDigite o nome: ");
+		              scanf("%s", &nome[linha]);
+		              printf("\ndigite o cpf:");
+		              scanf("%d", &cpf[linha]);
+                  printf("\nFileira: %d%d ", lin, col);
+                  printf("\nDados gravados com sucesso!");
+                printf("");
+                  }
+                  
+                  else
+                  {
+                    printf("******ATENÇÃO! CADEIRA OCUPADA!******\n\n");
                     }
-                  
-                  
-                  printf("\n");
-              }
-          printf("\t              |   |   |   |   |   |   |   |   |   |\n");
-          printf("\t     Coluna   0   1   2   3   4   5   6   7   8   9 \n");
-          printf("\t_____________________________________________________\n\n");
-          printf("\n\n");
-          }
-
-          if (op1==1){
-              printf("\nFileira: ");
-              scanf("%d", &lin);
-              printf("\nColuna: ");
-              scanf("%d",&col);
-            if  (lin>=0 & lin<2 && col<10)
+                    }
+                    else
+                    {
+                      printf("\n******CADEIRA NÃO DISPONÍVEL!******");
+                      }
+                      printf("\n");
+                      printf("------------------------------------------------\n");
+                      printf("\t\t\t\t________________________________________\n");
+                      printf("\t\t\t\t      |                         |\n");
+                      printf("\t\t\t\t      |          PALCO          |\n");
+                      printf("\t\t\t\t      |_________________________|\n\n");
+                      
+                      for(lin=0 ; lin<10; lin++){
+                        printf("\t ");
+                        printf("Fileira %d ->",lin);
+                        for(col=0 ; col<10 ; col++){
+                          if(assento[lin] [col]==0){
+                            printf("|-| ");
+                            }else
+                            printf("|%s%d| ", nome[linha], cpf[linha]);
+                            }
+                            
+                            printf("\n");
+                            }
+                            printf("\t              |   |   |   |   |   |   |   |   |   |\n");
+                            printf("\t     Coluna   0   1   2   3   4   5   6   7   8   9 \n");
+                            printf("\t_____________________________________________________\n\n");
+                            printf("\n\n");
+                            }
+  if (op1==1){
+  printf("\nFileira: ");
+  scanf("%d", &lin);
+  printf("\nColuna: ");
+  scanf("%d",&col);
+  
+  if  (lin>=0 & lin<2 && col<10)
             {
               if (assento [lin] [col]==0)
               {
+                
                 assento [lin] [col]=1;
                 printf("\nDigite o nome: ");
 		            scanf("%s", &nome[linha]);
 		            printf("\ndigite o cpf:");
 		            scanf("%d", &cpf[linha]);
-                printf("");              
+                
+                printf("\nFileira: %d%d ", lin, col);
+                
                 printf("\nDados gravados com sucesso!");
+
                 }            
 						else
 						{
@@ -160,29 +165,30 @@ do{//system permite executar o prompt dentro do programa
 
 
 
-           printf("\t\t\t\t________________________________________\n");
-  printf("\t\t\t\t      |                         |\n");
-  printf("\t\t\t\t      |          PALCO          |\n");
-  printf("\t\t\t\t      |_________________________|\n\n");
-
-          		for(f=0 ; f<10; f++){
-									printf("\t ");
-									printf("Fileira %d ->",f);
-                  for(c=0 ; c<10 ; c++){
-									if(assento[f] [c]==0){
-                    printf("|-| ");
-                    }else
-                    printf("|X| ");
-                    }
-                  
-                  
-                  printf("\n");
-              }
-          printf("\t              |   |   |   |   |   |   |   |   |   |\n");
-          printf("\t     Coluna   0   1   2   3   4   5   6   7   8   9 \n");
-          printf("\t_____________________________________________________\n\n");
-          printf("\n\n");
-          }
+           printf("\n");
+                      printf("------------------------------------------------\n");
+                      printf("\t\t\t\t________________________________________\n");
+                      printf("\t\t\t\t      |                         |\n");
+                      printf("\t\t\t\t      |          PALCO          |\n");
+                      printf("\t\t\t\t      |_________________________|\n\n");
+                      
+                      for(lin=0 ; lin<10; lin++){
+                        printf("\t ");
+                        printf("Fileira %d ->",lin);
+                        for(col=0 ; col<10 ; col++){
+                          if(assento[lin] [col]==0){
+                            printf("|-| ");
+                            }else
+                            printf("|%d%d| ", lin, col);
+                            }
+                            
+                            printf("\n");
+                            }
+                            printf("\t              |   |   |   |   |   |   |   |   |   |\n");
+                            printf("\t     Coluna   0   1   2   3   4   5   6   7   8   9 \n");
+                            printf("\t_____________________________________________________\n\n");
+                            printf("\n\n");
+                            }
 
 
 
@@ -190,18 +196,26 @@ do{//system permite executar o prompt dentro do programa
 
 		printf("\nDigite 1 para continuar cadastrando, ou 0 para voltar ao menu.\n");
 		scanf("%d",&op);
-		linha++; //proxima execição muda para proxima linha ou (linha=linha+1)
+		linha++;
+
+    //proxima execição muda para proxima linha ou (linha=linha+1)
 	}while(op==1);
 	    		break;
 
 	    	case 2:
         printf("\nDigite o CPF:");
 				scanf("%d", &cpfPesquisa);
+        
 
 				for(i=0;i<SIZE;i++)
+        
 					if(cpf[i]==cpfPesquisa) {
-						printf("\nNome: %s\nCPF: %d", nome[i], cpf[i]);
-						}
+						printf("\nNome: %s\nCPF: %d\n", nome[i], cpf[i]);
+            printf("%d", s[i]);
+            }
+            
+
+                            
 
 				break;
 
